79c881d4ac08c052fe158f9abcde4a62fef79c75
FernandoCagale <fernando.b.cagale@gmail.com>
2019-07-16 20:18:48 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# poc-grpc
+#GO poc-gprc 
+
+```sh
+$ protoc -I helloworld/ helloworld/helloworld.proto --go_out=plugins=grpc:helloworld
+```
+
+```sh
+$ go run server/server.go
+```
+
+```sh
+$ go run client/client.go
+$ Greeting: Hello world
+```
---
+++ client/client.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/FernandoCagale/poc-grpc/helloworld"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Greeting: %s", r.Message)
+}
